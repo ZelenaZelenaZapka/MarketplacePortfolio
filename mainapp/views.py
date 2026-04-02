79d5b3af2f6e0b191d66a9884a1150b05485acda
ZelenaZelenaZapka <zelenazapka@gmail.com>
2026-04-02 12:14:06 +0000

    fetch: split startpage on moduls

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from .models import Product
 
 def index(request):
-    return render(request, "startpage.html")
+    return render(request, "page_of_store/startpage.html")
 
-
+# def product_list(request):
+#     product = Product.objects.all()
+#     return render(request, )
