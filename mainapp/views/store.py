9951356453ac3b7ea5a4fca3c2f9c2304182fd81
ZelenaZelenaZapka <zelenazapka@gmail.com>
2026-04-08 12:24:54 +0000

    feat: add carts of items on store page

--- mainapp/views/store.py
+++ mainapp/views/store.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from ..models import Product
 
 def store_page(request):
-    return render(request, "page_of_store/startpage.html")
+    products = Product.objects.all()
+    return render(request, "page_of_store/startpage.html", {'products': products})
 
 def dashboard(request):
     return render (request, "page_of_store/dashboard.html")
