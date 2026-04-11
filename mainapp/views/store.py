590b958678b681f66c074171d978e816756f04d7
ZelenaZelenaZapka <zelenazapka@gmail.com>
2026-04-11 21:22:43 +0000

    fix: fixed bug about cart item on store page, button of deal now work

--- mainapp/views/store.py
+++ mainapp/views/store.py
@@ -1,11 +1,12 @@
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from ..models import Product, Customer
+from .order import get_cart_data
 
 
 def store_page(request):
     products = Product.objects.all()
-    return render(request, "page_of_store/startpage.html", {'products': products})
+    return render(request, "page_of_store/startpage.html", {'products': products, 'cart_item': get_cart_data(request)})
 
 
 def dashboard(request):
